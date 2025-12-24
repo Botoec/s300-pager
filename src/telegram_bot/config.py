80f00dfe9f22928b48ad40f36e8a85d8daee6d17
Maxim <vma@eis24.me>
2025-12-25 01:01:45 +0300

    wip

--- src/telegram_bot/config.py
+++ src/telegram_bot/config.py
@@ -7,7 +7,7 @@ class Settings(BaseSettings):
     KAFKA_TOPIC: list[str] = ["qr_authentication"]
     GW_API_URL: str = "http://192.168.10.251:8084"  # Бэкенд для проверок
     REDIS_URL: str = "redis://redis:6379/0"     # Для auth_token
-    WEBAPP_URL: str = "https://olid-roger-furnitureless.ngrok-free.dev/index.html"
+    WEBAPP_URL: str = "https://mz.nanitor.ru/"
     PHONE_RECHECK_INTERVAL: int = 30  # Дни для перепроверки номера
     PHONE_RECHECK_COUNT: int = 10     # Каждый N вход
     BOT_USERNAME: str = "s300-pager-bot"   # Для QR URL
